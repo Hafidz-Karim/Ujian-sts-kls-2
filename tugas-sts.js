c259ffc3a301b0e93d40883343b2993b23234119
Hafidz <hafidzkarim18@gmail.com>
2024-10-07 10:58:05 +0700

    commit ke 4

--- tugas-sts.js
+++ tugas-sts.js
@@ -24,3 +24,43 @@ function validateAddres(addres) {
   }
 }
 validateAddres("Kota Salatiga");
+
+// Tugas 3
+function validatePhoneNumber(phoneNumber) {
+
+  if (phoneNumber === "") {
+      console.log("Nomor telepon tidak boleh kosong");
+      return;
+  }
+  phoneNumber = phoneNumber.toString();
+
+  if (!isNaN(phoneNumber)) {
+      if (phoneNumber.length >= 10 && phoneNumber.length <= 13) {
+          console.log("Nomor telepon valid");
+      } else {
+          console.log("Nomor telepon harus terdiri dari 10-13 angka");
+      }
+  } else {
+      console.log("Nomor telepon hanya boleh berisi angka");
+  }
+}
+
+validatePhoneNumber("9191639637");
+
+
+  // Tugas 4
+  function validateEmail(email) {
+    if (email === "") {
+        return "Email tidak boleh kosong";
+    }
+
+    if (!email.includes("@")) {
+        return "Email harus mengandung karakter '@'";
+    }
+    if (email.length < 5) {
+        return "Email terlalu pendek";
+    }
+    return "Email valid";
+}
+const email = "Hafidzkarim17@gmail.com";
+console.log(validateEmail(email));
