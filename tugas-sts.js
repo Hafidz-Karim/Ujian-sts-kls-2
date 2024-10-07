d93d0608249f14e4db2b4b5c70ff9f91f6afcaff
Hafidz <hafidzkarim18@gmail.com>
2024-10-07 10:50:02 +0700

    commit ke 3

--- tugas-sts.js
+++ tugas-sts.js
@@ -1,32 +1,26 @@
- // Validasi nama
- let name = "";
- function validateName(name) {
+// Validasi nama
+let name = "";
+function validateName(name) {
+  if (name.length === 3) {
+    console.log("Valid");
+  } else if ((name.length <= 2) & (name !== 0)) {
+    console.log("Nama minimal terdiri dari 3 huruf");
+  } else {
+    console.log("Nama tidak valid dan nama harus diisi");
+  }
+}
+validateName("Ali");
 
-     if (name.length === 3) {
-         document.write("Valid")
-     }
-     else if (name.length <= 2 & name !== 0) {
-         document.write("Nama minimal terdiri dari 3 huruf")
-     }
-     else {
-         document.write("Nama tidak valid dan nama harus diisi");
-     }
- }
- validateName('Ali');
- document.write("<br>");
-
-
- // tugas 2
- let addres = "";
- function validateAddres(addres) {
-     if (addres.length >= 10) {
-         document.write("Valid");
-     }
-     else if (addres.length <= 9 & addres.length !== 0) {
-         document.write("Alamat terlalu pendek dan tidak valid");
-     }
-     else {
-         document.write("Alamat Invalid")
-     }
- }
- validateAddres('Kota Salatiga');
+// tugas 2
+let addres = "";
+function validateAddres(addres) {
+  if (addres.length >= 10) {
+    console.log("Valid");
+  } 
+  else if ((addres.length <= 9) & (addres.length !== 0)) {
+    console.log("Alamat terlalu pendek dan tidak valid");
+  } else {
+    console.log()("Alamat Invalid");
+  }
+}
+validateAddres("Kota Salatiga");
